7ecab6ae17a5301d4f7ed3e36ddbc9c8decbe77c
Alexander Moore <alexandermoore202@gmail.com>
2025-09-27 21:39:50 -0400

    Added in side panel and contact card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
+import RecommendationCard from "./ui/RecommendationCard";
+
 export default function Home() {
-  return <div className="font-sans">Test</div>;
+  return (
+    <div className="w-[400px] bg-[#151414] h-screen flex flex-col border-r border-white/5">
+      <div className="p-4 w-full border-b border-white/5">
+        <h1 className="font-title text-white text-4xl/[48px]">My Collection</h1>
+      </div>
+      <div className="w-full p-4 flex flex-col justify-between">
+        <RecommendationCard />
+      </div>
+    </div>
+  );
 }
