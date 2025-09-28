071c8a6f717487afcc9d47b42ab401f7ea48fa3d
Alexander Moore <alexandermoore202@gmail.com>
2025-09-27 22:37:41 -0400

    Minor styling update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import RecordResult from "./ui/RecordResult";
 export default function Home() {
   return (
     <div className="flex">
-      <div className="w-[400px] bg-[#151414] h-screen flex flex-col border-r border-white/5">
+      <div className="w-[500px] bg-[#151414] h-screen flex flex-col border-r border-white/5">
         <div className="p-4 w-full border-b border-white/5">
           <h1 className="font-title text-white text-5xl">My Collection</h1>
         </div>
