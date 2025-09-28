a241592af13ea0140964778801d5dae1624d720e
Alexander Moore <alexandermoore202@gmail.com>
2025-09-27 22:30:51 -0400

    Added a record result

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,24 @@
 import RecommendationCard from "./ui/RecommendationCard";
+import RecordResult from "./ui/RecordResult";
 
 export default function Home() {
   return (
-    <div className="w-[400px] bg-[#151414] h-screen flex flex-col border-r border-white/5">
-      <div className="p-4 w-full border-b border-white/5">
-        <h1 className="font-title text-white text-4xl/[48px]">My Collection</h1>
+    <div className="flex">
+      <div className="w-[400px] bg-[#151414] h-screen flex flex-col border-r border-white/5">
+        <div className="p-4 w-full border-b border-white/5">
+          <h1 className="font-title text-white text-5xl">My Collection</h1>
+        </div>
+        <div className="w-full p-4 flex flex-col justify-between">
+          <RecommendationCard />
+        </div>
       </div>
-      <div className="w-full p-4 flex flex-col justify-between">
-        <RecommendationCard />
+      <div className="w-full h-screen flex flex-col gap-4 p-8">
+        <RecordResult
+          imageString="/alligator.webp"
+          title="Alligator"
+          artist="The National"
+          year="2005"
+        />
       </div>
     </div>
   );
---
+++ src/app/ui/RecordResult.tsx
@@ -0,0 +1,44 @@
+import Image from "next/image";
+
+interface RecordResultProps {
+  imageString: string;
+  title: string;
+  artist: string;
+  year: string;
+}
+
+export default function RecordResult({
+  imageString,
+  title,
+  artist,
+  year,
+}: RecordResultProps) {
+  return (
+    <div className="flex flex-col items-center gap-3">
+      <div className="relative">
+        <Image
+          src={imageString}
+          alt={`${title} album cover`}
+          width={294}
+          height={294}
+          className="blur-[5px] opacity-10 z-0"
+        />
+        <Image
+          src={imageString}
+          alt={`${title} album cover`}
+          width={278}
+          height={278}
+          className="z-10 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+        />
+      </div>
+      <div className="flex flex-col gap-2 px-4">
+        <h2 className="font-sans text-4xl font-semibold text-center">
+          {title}
+        </h2>
+        <p className="font-sans text-white font-light text-lg text-center">
+          {artist} &#183; {year}
+        </p>
+      </div>
+    </div>
+  );
+}
