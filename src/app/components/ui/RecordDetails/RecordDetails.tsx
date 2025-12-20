6758c7788de68c9aa0c36e321bc1fdd50a42e022
Alexander Moore <alexandermoore202@gmail.com>
2025-12-20 00:21:17 -0500

    Responsiveness improvements

--- src/app/components/ui/RecordDetails/RecordDetails.tsx
+++ src/app/components/ui/RecordDetails/RecordDetails.tsx
@@ -31,13 +31,13 @@ export default function RecordDetails({
       className="absolute left-0 top-0 z-50 w-full h-screen flex flex-col items-center bg-[#191818] xl:pt-10 pt-5 overflow-y-scroll scrollbar-hide"
     >
       <button
-        className="absolute top-6 right-10 flex items-center z-20 p-2 rounded-lg hover:bg-white/[0.04] transition duration-100 cursor-pointer"
+        className="fixed top-24 lg:top-10 right-4 lg:right-10 flex items-center z-20 p-2 rounded-lg hover:bg-white/[0.04] transition duration-100 cursor-pointer"
         onClick={onClose}
       >
         <CloseIcon className="text-[#C4C4C4] size-10" />
       </button>
-      <div className="w-full max-w-[1100px] flex flex-col xl:gap-10 gap-4 h-full px-4">
-        <div className="flex items-center gap-12">
+      <div className="w-full max-w-[1100px] flex flex-col xl:gap-10 gap-4 px-4">
+        <div className="flex flex-col lg:flex-row items-start p-4 lg:items-center gap-8 lg:gap-12">
           <div className="relative w-1/2">
             <Record
               colour={albumData.recordColour || ""}
@@ -47,7 +47,7 @@ export default function RecordDetails({
             <motion.div
               layoutId={`image-${albumData.imageString}`}
               transition={{ duration: 0.3, bounce: 0, type: "spring" }}
-              className="size-[320px] aspect-square absolute top-0 mr-[-120px] shadow-[4px_0_4px_0_rgba(0,0,0,0.25)]"
+              className="size-[200px] lg:size-[320px] aspect-square absolute top-0 mr-[-120px] shadow-[4px_0_4px_0_rgba(0,0,0,0.25)]"
             >
               <Image
                 src={albumData.imageString}
@@ -69,7 +69,7 @@ export default function RecordDetails({
             highlightColor={albumData.recordHighlightColour || ""}
           />
         </div>
-        <div className="grid grid-cols-2 gap-8 pb-8">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-x-8 gap-y-2 mb-40">
           {albumData.sides?.map((side) => (
             <TrackListSide
               key={side.side}
